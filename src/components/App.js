384c9ad2bddb738e0a582690f8fa413f16f452d4
AnkitKumarDelta <ankitna355@gmail.com>
2025-12-18 23:47:16 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  // const[width_percentage, setWidth_percentage] = useState(0);
+  const[innerWidth, setInnerWidth] = useState(0);
+
+  const intervalId = setTimeout(()=>{
+    let x = innerWidth+10;
+    if(x>100){
+      clearTimeout(intervalId);
+    }
+   else setInnerWidth(x);
+  },1000);
+  
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Progress Bar</h1>
+        <div id="barOuter">
+          <div id="barInner" style={{backgroundColor:"#0000FF",height:"100%",width:`${innerWidth}%`}}></div>
+        </div>
+        <p>{`${innerWidth}%`}</p>
     </div>
   )
 }
