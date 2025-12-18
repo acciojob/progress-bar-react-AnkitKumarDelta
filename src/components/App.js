4219151970784f10ecf43aa4874cb913b82b609a
AnkitKumarDelta <ankitna355@gmail.com>
2025-12-19 00:02:11 +0530

    save changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,30 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  // const[width_percentage, setWidth_percentage] = useState(0);
-  const[innerWidth, setInnerWidth] = useState(0);
+  const [innerWidth, setInnerWidth] = useState(0);
 
-  const intervalId = setTimeout(()=>{
-    let x = innerWidth+10;
-    if(x>100){
-      clearTimeout(intervalId);
-    }
-   else setInnerWidth(x);
-  },1000);
-  
+  if (innerWidth < 100) {
+    setTimeout(() => {
+      setInnerWidth(innerWidth + 10);
+    }, 1000);
+  }
+
   return (
     <div>
       <h1>Progress Bar</h1>
-        <div id="barOuter">
-          <div id="barInner" style={{backgroundColor:"#0000FF",height:"100%",width:`${innerWidth}%`}}></div>
-        </div>
-        <p>{`${innerWidth}%`}</p>
+      <div id="barOuter">
+        <div
+          id="barInner"
+          style={{
+            backgroundColor: "#0000FF",
+            height: "100%",
+            width: `${innerWidth}px`
+          }}
+        ></div>
+      </div>
+      <p>{innerWidth}%</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
